e9546d3328a215a3bc549d3ab8e337d21e0ac221
deusvult6666 <hibari1999@gmail.com>
2023-02-17 21:08:02 +0200

    unit commit

--- calculator/evaluation.py
+++ calculator/evaluation.py
@@ -1,11 +1,11 @@
 def evaluate(x,y,z):
     if z == 1:
-        result = x + y
+        j = x + y
     elif z == 2:
-        result = x - y
+        j = x - y
     elif z == 3:
-        result = x * y
+        j = x * y
     elif z == 4:
-        result = x / y
+        j = x / y
         
-    return result
+    return j
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from calculator.evaluation import evaluate
+from convertor import conv_1
 
 
 print("1.Add")
@@ -6,9 +7,29 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-z = int(input('Enter operation from 1 to 4'))
-x = int(input('Enter fisrt number:'))
-y = int(input('Enter second number:'))
+print("5. base 10")
+print("6. base 02")
+print("7. base 12")
+print("8. base 16")
+
+try:
+    z = int(input('Enter operation from 1 to 4'))
+    k = int(input("Enter number systems from 5 to 8"))
+except ValueError:
+    z == 0
+    print("Dont print zero")
+ 
+try:
+    x = int(input('Enter fisrt number:'))
+    y = int(input('Enter second number:'))
+    
+except ValueError:
+    print("Enter only integer please")
+
+j = None
+bs_3 = None
+
+
 
 
 print(evaluate(x,y,z))
