b32947adb7580e68f65dddcb5cf2b2ea64fbaa7e
deusvult6666 <hibari1999@gmail.com>
2023-02-17 22:04:59 +0200

    init commit

--- convertor.py
+++ convertor.py
@@ -2,19 +2,19 @@ from bases import Bases
 from calculator.evaluation import evaluate
 bases = Bases()
 
-def conv_1(k, bs_1, bs_2, bs_3 : str) -> int:
-    if k >= 5:
+def conv_1(bs_1, bs_2, bs_3 : str) -> int:
+    if i >= 5:
         bs_1 = input(("Enter 1st value").lower)
         bs_2 = input(("Enter 2nd value").lower)
-    if k == 6:
+    if i == 6:
         x = bases.fromBase2(bs_1)
         y = bases.fromBase2(bs_2)
         bs_3 = bases.toBase2(evaluate(x, y, z))
-    elif k == 7:
+    elif i == 7:
         x = bases.fromBase8(bs_1)
         y = bases.fromBase8(bs_2)
         bs_3 = bases.toBase8(evaluate(x, y, z))
-    elif  k == 8:
+    elif  i == 8:
         x = bases.fromBase16(bs_1)
         y = bases.fromBase16(bs_2)
         bs_3 = bases.toBase16(evaluate(x, y, z))
